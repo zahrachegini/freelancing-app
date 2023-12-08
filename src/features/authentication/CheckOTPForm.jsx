d849492e0a7101375956e595f0ccecd9020b8977
zahrachegini <zch6511@gmail.com>
2023-12-08 16:44:37 +0330

    First commit

---
+++ src/features/authentication/CheckOTPForm.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import OTPInput from "react-otp-input";
+
+function CheckOTPForm() {
+  const [otp, setOtp] = useState("");
+  return (
+    <div>
+      <form action="" className="space-y-10">
+        <p className="font-bold text-secondary-800">کد تائید را وارد کنید</p>
+        <OTPInput
+          value={otp}
+          onChange={setOtp}
+          numInputs={6}
+          renderSeparator={<span>-</span>}
+          renderInput={(props) => <input type="number" {...props} />}
+          containerStyle="flex flex-row-reverse gap-x-2 justify-center"
+          inputStyle={{
+            width: "2.5rem",
+            padding: "0.5rem 0.2rem",
+            border: "1px solid rgb(var(--color-primary-300))",
+            borderRadius: "0.5rem",
+          }}
+        />
+        <button className="btn btn--primary w-full">تائید</button>
+      </form>
+    </div>
+  );
+}
+
+export default CheckOTPForm;
