b1e033866fb540594ef5427194b93279c530ba08
zahrachegini <zch6511@gmail.com>
2023-12-08 23:52:37 +0330

    SendOTPform

--- src/features/authentication/SendOTPForm.jsx
+++ src/features/authentication/SendOTPForm.jsx
@@ -1,18 +1,39 @@
 import { useState } from "react";
 import TextField from "../../ui/TextField";
+import { useMutation } from "@tanstack/react-query";
+import { getOtp } from "../../services/authService";
+import toast from "react-hot-toast";
 
 function SendOTPForm() {
   const [phoneNumber, setPhoneNumber] = useState("");
+
+  const { isPending, error, data, mutateAsync } = useMutation({
+    mutationFn: getOtp,
+  });
+
+  const sendOtoHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const data = await mutateAsync({ phoneNumber });
+      console.log(data);
+      toast.success(data.message);
+    } catch (error) {
+      toast.error(error?.response?.data?.message);
+    }
+  };
+
   return (
     <div>
-      <form action="" className="space-y-8">
+      <form action="" className="space-y-8" onSubmit={sendOtoHandler}>
         <TextField
           label="شماره موبایل"
           name={phoneNumber}
           value={phoneNumber}
           onChange={(e) => setPhoneNumber(e.target.value)}
         />
-        <button className="btn btn--primary w-full">ارسال کد تائید</button>
+        <button type="submit" className="btn btn--primary w-full">
+          ارسال کد تائید
+        </button>
       </form>
     </div>
   );
--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,12 +1,12 @@
 import CheckOTPForm from "../features/authentication/CheckOTPForm";
-// import SendOTPForm from "../features/authentication/SendOTPForm";
+import SendOTPForm from "../features/authentication/SendOTPForm";
 
 function Auth() {
   return (
     <div className="flex justify-center pt-10">
       <div className="w-full sm:max-w-sm">
-        {/* <SendOTPForm /> */}
-        <CheckOTPForm />
+        <SendOTPForm />
+        {/* <CheckOTPForm /> */}
       </div>
     </div>
   );
